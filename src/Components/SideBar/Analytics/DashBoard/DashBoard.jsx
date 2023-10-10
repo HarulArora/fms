4190a154b9f7acb2a46bb7d7a869f748557c924f
HarulArora <harularora463@gmail.com>
2023-10-10 19:02:22 +0530

    Fee Bottom nav buttons

--- src/Components/SideBar/Analytics/DashBoard/DashBoard.jsx
+++ src/Components/SideBar/Analytics/DashBoard/DashBoard.jsx
@@ -6,48 +6,48 @@ import Graph from "./Graphs/Graph";
 import BarGraph from "./Graphs/BarGraph";
 import PieGraph from "./Graphs/PieGraph";
 function DashBoard() {
-  return (
-    <>
-      <div className="p-5 dark:bg-red-300 bg-white">
-        <div>
-          <Breadcrumb category="ANALYTICS" page="Dashboard" />
-        </div>
-        <div className="grid xl:grid-cols-4 gap-x-3 md:grid-cols-2 gap-y-3 mb-10 mt-8">
-          <Card
-            heading="Students"
-            number="4000"
-            percentage="12% more than last Year"
-          />
-          <Card
-            heading="Teacher"
-            number="339"
-            percentage="27% less than last Year"
-          />
-          <Card
-            heading="Teacher"
-            number="10"
-            percentage="20% more than last month"
-          />
-          <Card
-            heading="Revenue"
-            number="₹6900000"
-            percentage="34% more than last Year"
-          />
-        </div>
-        <div className="w-auto">
-          <Graph />
-        </div>
-        <div className="grid grid-cols-2">
-          <div>
-            <BarGraph />
-          </div>
-          <div>
-            <PieGraph />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="p-5 dark:bg-red-300 bg-white w-full">
+                <div>
+                    <Breadcrumb category="ANALYTICS" page="Dashboard" />
+                </div>
+                <div className="grid xl:grid-cols-4 gap-x-3 md:grid-cols-2 gap-y-3 mb-10 mt-8">
+                    <Card
+                        heading="Students"
+                        number="4000"
+                        percentage="12% more than last Year"
+                    />
+                    <Card
+                        heading="Teacher"
+                        number="339"
+                        percentage="27% less than last Year"
+                    />
+                    <Card
+                        heading="Teacher"
+                        number="10"
+                        percentage="20% more than last Year"
+                    />
+                    <Card
+                        heading="Revenue"
+                        number="₹6900000"
+                        percentage="34% more than last Year"
+                    />
+                </div>
+                <div className="w-auto">
+                    <Graph />
+                </div>
+                <div className="grid grid-cols-2">
+                    <div>
+                        <BarGraph />
+                    </div>
+                    <div>
+                        <PieGraph />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default DashBoard;
--- src/Components/SideBar/Analytics/Fee/Fee.jsx
+++ src/Components/SideBar/Analytics/Fee/Fee.jsx
@@ -1,21 +1,24 @@
-import React from "react";
-import Breadcrumb from "../../Breadcrumb/Breadcrumb";
-import StudentList from "./Student List/StudentList";
+import React from 'react'
+import Breadcrumb from '../../Breadcrumb/Breadcrumb'
+import StudentList from './Student List/StudentList'
+import BottomNavigation from './Bottom Navigation/BottomNavigation'
 // import { Link, NavLink } from 'react-router-dom'
 
 function Fee() {
-  return (
-    <>
-      <div className="h-auto w-full overflow-y-hidden">
-        <div className="flex-grow p-5">
-          <Breadcrumb category="ANALYTICS" page="Fees" />
+    return (
+        <div>
+
+            <div className='flex-grow p-5'>
+                <Breadcrumb category="ANALYTICS" page="Fees" />
+            </div>
+            <div>
+                <StudentList></StudentList>
+            </div>
+            <div>
+                <BottomNavigation></BottomNavigation>
+            </div>
         </div>
-        {/* <div> */}
-        <StudentList></StudentList>
-        {/* </div> */}
-      </div>
-    </>
-  );
+    )
 }
 
-export default Fee;
+export default Fee
