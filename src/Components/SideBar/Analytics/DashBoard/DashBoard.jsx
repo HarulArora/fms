4d01f05d5d7aa9d07142f44f9d9d2814506efe82
HarulArora <harularora463@gmail.com>
2023-10-10 13:07:55 +0530

    Index.css comment

--- src/Components/SideBar/Analytics/DashBoard/DashBoard.jsx
+++ src/Components/SideBar/Analytics/DashBoard/DashBoard.jsx
@@ -9,11 +9,11 @@ function DashBoard() {
                 <div>
                     <Breadcrumb category="ANALYTICS" page="Dashboard" />
                 </div>
-                <div className='flex gap-11 mt-5'>
+                <div className='flex gap-11 mt-5 flex-wrap'>
                     <Card heading="Students" number="4000" percentage="12% more than last Year" />
                     <Card heading="Teacher" number="339" percentage="27% less than last Year" />
-                    <Card heading="Revenue" number="₹6900000" percentage="34% more than last Year" />
                     <Card heading="Teacher" number="10" percentage="20% more than last month" />
+                    <Card heading="Revenue" number="₹6900000" percentage="34% more than last Year" />
                 </div>
             </div >
         </>
