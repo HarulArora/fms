c905c1c3bafed8a87984c45ccf4b7b6c958c9bfd
HarulArora <harularora463@gmail.com>
2023-10-10 15:22:49 +0530

    Fees Page

--- src/Components/SideBar/Analytics/Fee/Fee.jsx
+++ src/Components/SideBar/Analytics/Fee/Fee.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Breadcrumb from '../../Breadcrumb/Breadcrumb'
+import StudentList from './Student List/StudentList'
 // import { Link, NavLink } from 'react-router-dom'
 
 function Fee() {
     return (
         <>
-            <div className='flex-grow p-5'>
-                <Breadcrumb category="ANALYTICS" page="Fees" />
+            <div>
+
+                <div className='flex-grow p-5'>
+                    <Breadcrumb category="ANALYTICS" page="Fees" />
+                </div>
+                <div>
+                    <StudentList></StudentList>
+                </div>
             </div>
         </>
     )
