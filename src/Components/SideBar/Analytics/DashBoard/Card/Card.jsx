67bd25def041e7f2114a7b1758eb511f12410a85
Hashir <hashir0601.be21@chitkara.edu.in>
2023-10-10 13:15:48 +0530

    changes in dashboard layout

--- src/Components/SideBar/Analytics/DashBoard/Card/Card.jsx
+++ src/Components/SideBar/Analytics/DashBoard/Card/Card.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React from "react";
 
 function Card({ heading, number, percentage }) {
-    return (
-        <div className="stats shadow">
-            <div className="stat w-96 h-48">
-                <div className="stat-title flex justify-center align-middle">{heading}</div>
-                <div className="stat-value flex justify-center align-middle">{number}</div>
-                <div className="stat-desc flex justify-center align-middle">{percentage}</div>
-            </div>
+  return (
+    <div className="stats shadow">
+      <div className="stat w-[220.55px] h-[40%]">
+        <div className="stat-title flex justify-center align-middle">
+          {heading}
         </div>
-    )
+        <div className="stat-value flex justify-center align-middle">
+          {number}
+        </div>
+        <div className="stat-desc flex justify-center align-middle">
+          {percentage}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
