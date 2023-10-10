7bd446c890716d2e5c292ec0288e44b2ebf9053b
Hashir <hashir0601.be21@chitkara.edu.in>
2023-10-10 20:01:58 +0530

    graph error solved

--- src/Components/SideBar/Analytics/DashBoard/Card/Card.jsx
+++ src/Components/SideBar/Analytics/DashBoard/Card/Card.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Card({ heading, number, percentage }) {
   return (
-    <div className="stats shadow dark:bg-red-300 bg-black">
-      <div className="stat w-[220.55px] h-[40%]">
+    <div className="stats shadow dark:bg-red-300 bg-black h-[150px] flex justify-center items-center">
+      <div className="stat w-[220.55px] h-[80px] flex flex-col items-center mb-12">
         <div className="stat-title flex justify-center align-middle">
           {heading}
         </div>
