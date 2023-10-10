1299b26675dbb7fc8bf079d758ced0a8d8ec259e
Hashir <hashir0601.be21@chitkara.edu.in>
2023-10-10 20:13:06 +0530

    removal of some classes in Card.jsx

--- src/Components/SideBar/Analytics/DashBoard/Card/Card.jsx
+++ src/Components/SideBar/Analytics/DashBoard/Card/Card.jsx
@@ -4,15 +4,9 @@ function Card({ heading, number, percentage }) {
   return (
     <div className="stats shadow dark:bg-red-300 bg-black h-[150px] flex justify-center items-center">
       <div className="stat w-[220.55px] h-[80px] flex flex-col items-center mb-12">
-        <div className="stat-title flex justify-center align-middle">
-          {heading}
-        </div>
-        <div className="stat-value flex justify-center align-middle">
-          {number}
-        </div>
-        <div className="stat-desc flex justify-center align-middle">
-          {percentage}
-        </div>
+        <div className="stat-title">{heading}</div>
+        <div className="stat-value ">{number}</div>
+        <div className="stat-desc">{percentage}</div>
       </div>
     </div>
   );
